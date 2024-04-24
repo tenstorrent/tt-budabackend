a80a3751738826a7524df95a02d7cdcd553f2d16
Kristijan Mitrovic <kmitrovic@tenstorrent.com>
2024-04-24 12:43:21 +0000

    Updated coordiante conversion helper script
    * Updated coordiante conversion helper script
    
    (cherry picked from commit 52f197ccd7e0998b969817f6d22b068e4ae4807c)

--- verif/template_netlist/scripts/utility/coordinate_conversion.py
+++ verif/template_netlist/scripts/utility/coordinate_conversion.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+import os
 from dataclasses import dataclass
+
+from ruamel.yaml import YAML
 
 
 @dataclass
@@ -12,12 +15,15 @@ class CoreLocation:
     def __str__(self) -> str:
         return f"(x = {self.x}, y = {self.y})"
 
+    def convert_to_netlist_coordinates(self) -> NetlistCoordinates:
+        return NetlistCoordinates(self.y, self.x)
+
 
 @dataclass
 class NetlistCoordinates:
-    def __init__(self, core_logical_location: CoreLocation) -> None:
-        self.r = core_logical_location.y
-        self.c = core_logical_location.x
+    def __init__(self, r: int, c: int) -> None:
+        self.r = r
+        self.c = c
 
     def __str__(self) -> str:
         return f"(r = {self.r}, c = {self.c})"
@@ -76,12 +82,21 @@ class WorkerCores(ABC):
     def physical_to_netlist_coordinates(
         self, physical_location: CoreLocation
     ) -> NetlistCoordinates:
-        return NetlistCoordinates(
-            CoreLocation(
-                self.worker_physical_to_logical_x[physical_location.x],
-                self.worker_physical_to_logical_y[physical_location.y],
-            )
-        )
+        return CoreLocation(
+            self.worker_physical_to_logical_x[physical_location.x],
+            self.worker_physical_to_logical_y[physical_location.y],
+        ).convert_to_netlist_coordinates()
+
+    @staticmethod
+    def create_from_string(arch: str) -> WorkerCores:
+        if arch == "grayskull":
+            return GSWorkerCores()
+        elif arch == "wormhole" or arch == "wormhole_b0":
+            return WHWorkerCores()
+        elif arch == "blackhole":
+            return BHWorkerCores()
+        else:
+            raise RuntimeError(f"Invalid architecture {arch}")
 
 
 class GSWorkerCores(WorkerCores):
@@ -134,6 +149,13 @@ class SoCInfoConstants:
 
 class SoCInfo:
     @staticmethod
+    def is_harvested(core_location: CoreLocation) -> bool:
+        return (
+            core_location.x > SoCInfoConstants.wh_harvested_translation_offset
+            and core_location.y > SoCInfoConstants.wh_harvested_translation_offset
+        )
+
+    @staticmethod
     def convert_harvested_core_location_to_unharvested(
         harvested_core_location: CoreLocation,
     ) -> CoreLocation:
@@ -182,32 +204,124 @@ class SoCInfo:
         return harvested_core_location
 
 
-if __name__ == "__main__":
-    coords = [
-        CoreLocation(18, 18),
-        CoreLocation(19, 18),
-        CoreLocation(20, 18),
-        CoreLocation(21, 18),
-        CoreLocation(22, 18),
-        CoreLocation(23, 18),
-        CoreLocation(20, 21),
-        CoreLocation(21, 21),
-        CoreLocation(22, 21),
-        CoreLocation(23, 21),
-        CoreLocation(24, 21),
-        CoreLocation(19, 23),
-        CoreLocation(18, 25),
-        CoreLocation(19, 25),
-        CoreLocation(20, 25),
-        CoreLocation(21, 25),
-        CoreLocation(24, 27),
-        CoreLocation(25, 27),
-    ]
+class OpNode:
+    def __init__(self, op_name: str, graph_name: str, op_dict: dict) -> None:
+        self.op_name = op_name
+        self.graph_name = graph_name
+        self.op_type = op_dict["type"]
+        self.grid_loc = op_dict["grid_loc"]
+        self.grid_size = op_dict["grid_size"]
 
-    for coord in coords:
-        unharv = SoCInfo.convert_harvested_core_location_to_unharvested(coord)
-        wh = WHWorkerCores()
-        print(
-            wh.physical_to_netlist_coordinates(unharv)
-            == NetlistCoordinates(CoreLocation(coord.y - 18, coord.x - 18))
-        )
+    @property
+    def name(self) -> str:
+        return self.op_name
+
+    @property
+    def type(self) -> str:
+        return self.op_type
+
+    @property
+    def grid_loc_r(self) -> int:
+        return self.grid_loc[0]
+
+    @property
+    def grid_loc_c(self) -> int:
+        return self.grid_loc[1]
+
+    @property
+    def grid_size_r(self) -> int:
+        return self.grid_size[0]
+
+    @property
+    def grid_size_c(self) -> int:
+        return self.grid_size[1]
+
+    @property
+    def spanning_cores(self) -> list[NetlistCoordinates]:
+        cores = []
+        for r in range(self.grid_loc_r, self.grid_loc_r + self.grid_size_r):
+            for c in range(self.grid_loc_c, self.grid_loc_c + self.grid_size_c):
+                cores.append(NetlistCoordinates(r, c))
+
+        return cores
+
+
+class ProblematicCoreFinder:
+    def __init__(self, netlist_path: str) -> None:
+        # Yaml settings.
+        self.yaml = YAML(typ="rt")
+        self.yaml.width = 200
+        self.netlist_path = netlist_path
+        self.netlist_dict = self.read_netlist()
+        self.cores_to_ops_mapping = {}
+        # We support only one arch.
+        self.arch = self.netlist_dict["devices"]["arch"]
+        self.arch = self.arch[0] if isinstance(self.arch, list) else self.arch
+
+    def read_netlist(self) -> dict:
+        assert os.path.exists(self.netlist_path), f"Netlist file: '{self.netlist_path}' not found."
+
+        with open(self.netlist_path, "r") as file:
+            netlist_dict = self.yaml.load(file)
+
+        return netlist_dict
+
+    def map_cores_to_op(self, op: OpNode) -> None:
+        for core in op.spanning_cores:
+            if op.graph_name not in self.cores_to_ops_mapping:
+                self.cores_to_ops_mapping[op.graph_name] = {}
+
+            self.cores_to_ops_mapping[op.graph_name][str(core)] = op
+
+    def map_cores_to_ops(self) -> None:
+        for graph_name in self.netlist_dict["graphs"]:
+            for op_name in self.netlist_dict["graphs"][graph_name]:
+                if not self.is_op(op_name):
+                    continue
+
+                op = OpNode(op_name, graph_name, self.netlist_dict["graphs"][graph_name][op_name])
+                self.map_cores_to_op(op)
+
+    def is_op(self, op_name: str) -> bool:
+        return op_name != "target_device" and op_name != "input_count"
+
+    def find_ops(self, problematic_core: NetlistCoordinates) -> list[OpNode]:
+        ops = []
+        for graph_name in self.cores_to_ops_mapping.keys():
+            op = self.cores_to_ops_mapping[graph_name].get(str(problematic_core), None)
+            if op:
+                ops.append(op)
+
+        return ops
+
+    def convert_to_netlist_coordinates(
+        self, core_physical_location: CoreLocation
+    ) -> NetlistCoordinates:
+        if SoCInfo.is_harvested(core_physical_location):
+            core_physical_location = SoCInfo.convert_harvested_core_location_to_unharvested(
+                core_physical_location
+            )
+
+        converter = WorkerCores.create_from_string(self.arch)
+        return converter.physical_to_netlist_coordinates(core_physical_location)
+
+    def find(self, problematic_core_physical_location: CoreLocation) -> None:
+        problematic_core = self.convert_to_netlist_coordinates(problematic_core_physical_location)
+        print(f"Netlist coordinates of problematic core: {problematic_core}")
+
+        self.map_cores_to_ops()
+        problematic_ops = self.find_ops(problematic_core)
+
+        if len(problematic_ops) == 0:
+            print(f"Problematic core not found in any OP in netlist!")
+            return
+
+        for op in problematic_ops:
+            print(
+                f"OP {op.name} spans across problematic core {problematic_core} in epoch {op.graph_name}"
+            )
+
+
+if __name__ == "__main__":
+    finder = ProblematicCoreFinder("netlist.yaml")
+    finder.find(CoreLocation(x=19, y=25))
