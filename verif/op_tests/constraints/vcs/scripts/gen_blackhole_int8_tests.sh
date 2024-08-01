2dea4d5df867b824f053d1c5a4849e9a0ce36662
Slavko Krstic <skrstic@tenstorrent.com>
2024-08-01 09:08:11 +0000

    BH int nightly op tests
    * Nightly for int8 op tests
    
    (cherry picked from commit adba95e2f4e1761e8c4d98f52363de0444e369fa)

--- verif/op_tests/constraints/vcs/scripts/gen_blackhole_int8_tests.sh
+++ verif/op_tests/constraints/vcs/scripts/gen_blackhole_int8_tests.sh
@@ -7,6 +7,7 @@ cd ${vcs_dir}
 
 make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/unary/unary_single_int8_configs HARVESTED_ROWS=0 LOOPS=30 SEED=0 C=main.sv ARGS='+op=unary +int8=1'
 make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/binary/binary_single_int8_configs HARVESTED_ROWS=0 LOOPS=50 SEED=0 C=main.sv ARGS='+op=binary +int8=1'
+make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/binary/binary_sfpu_single_int8_configs HARVESTED_ROWS=0 LOOPS=20 SEED=0 C=main.sv ARGS='+op=binary_sfpu +int8=1'
 make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/reduce/reduce_dim_r_single_int8_configs HARVESTED_ROWS=0 LOOPS=5 SEED=0 C=main.sv ARGS='+op=reduce +reduce_dim=r +int8=1'
 make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/reduce/reduce_dim_c_single_int8_configs HARVESTED_ROWS=0 LOOPS=5 SEED=0 C=main.sv ARGS='+op=reduce +reduce_dim=c +int8=1'
 make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/reduce/reduce_dim_z_single_int8_configs HARVESTED_ROWS=0 LOOPS=5 SEED=0 C=main.sv ARGS='+op=reduce +reduce_dim=z +int8=1'
@@ -15,3 +16,4 @@ make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/depthwise/depth
 make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/matmul_ident/matmul_ident_single_int8_configs HARVESTED_ROWS=0 LOOPS=100 SEED=0 C=main.sv ARGS='+op=matmul_ident +int8=1'
 make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/matmul/matmul_acc_z_single_int8_configs HARVESTED_ROWS=0 LOOPS=30 SEED=0 C=main.sv ARGS='+op=matmul +accumulate_z=1 +int8=1'
 make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/matmul/matmul_single_int8_configs HARVESTED_ROWS=0 LOOPS=100 SEED=0 C=main.sv ARGS='+op=matmul +int8=1'
+make DEVICE=${device} TARGET_FILE=test_configs/blackhole/silicon/matmul/matmul_single_int8_quant_configs HARVESTED_ROWS=0 LOOPS=20 SEED=0 C=main.sv ARGS='+op=matmul +int8=1 +quantize=1'
