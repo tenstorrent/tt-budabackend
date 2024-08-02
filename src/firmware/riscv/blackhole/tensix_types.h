20489d4d410cb966394f6a0042aeb356195101c4
Radomir Djogo <rdjogo@tenstorrent.com>
2024-08-02 17:34:43 +0000

    Enable UInt8 math on BH
    (cherry picked from commit 8b0c27879857da8eecc9496f54eef7399422d9c9)

--- src/firmware/riscv/blackhole/tensix_types.h
+++ src/firmware/riscv/blackhole/tensix_types.h
@@ -253,11 +253,11 @@ enum class DataFormat : std::uint8_t
   Lf8       = 10,
   UInt16    = 12,
   Int8      = 14,
+  UInt8     = 30,
   Int32     = 8,
   Int16     = 9,
   Tf32      = 4,
   Fp8_e4m3  = 26,         //Not a valid HW encoding, it is Lf8 encoding + extra 5th bit set to specify Lf8 with E4M3
-  Uint8     = 129,        // Not a valid HW enum value, but useful to have it here for SW
   testMan7  = 0x82,       // intermediate format for testing: 7bit mantissa (6+hidden)
   testMan2  = 0x8A,       // intermediate format for testing: 2bit mantissa (2+hidden)
   Invalid   = 0xff
